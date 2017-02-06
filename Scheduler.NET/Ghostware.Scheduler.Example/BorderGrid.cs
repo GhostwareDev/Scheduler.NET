063e2bf3d0708bb1ce2c47ec4e7a83bb7c31d4ae
Kevin Goos <kevin.goos@infrabel.be>
2017-02-06 16:00:38 +0100

    Added clicking

--- Scheduler.NET/Ghostware.Scheduler.Example/BorderGrid.cs
+++ Scheduler.NET/Ghostware.Scheduler.Example/BorderGrid.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace Ghostware.Scheduler.Example
 {
     public class BorderGrid : Grid
     {
+        public BorderGrid()
+        {
+            Background = Brushes.Transparent;
+        }
+
         protected override void OnRender(DrawingContext dc)
         {
             double leftOffset = 0;
@@ -31,6 +32,39 @@ namespace Ghostware.Scheduler.Example
             }
 
             base.OnRender(dc);
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            var point = Mouse.GetPosition(this);
+
+            var row = 0;
+            var col = 0;
+            var accumulatedHeight = 0.0;
+            var accumulatedWidth = 0.0;
+
+            foreach (var rowDefinition in RowDefinitions)
+            {
+                accumulatedHeight += rowDefinition.ActualHeight;
+                if (accumulatedHeight >= point.Y)
+                    break;
+                row++;
+            }
+
+            foreach (var columnDefinition in ColumnDefinitions)
+            {
+                accumulatedWidth += columnDefinition.ActualWidth;
+                if (accumulatedWidth >= point.X)
+                    break;
+                col++;
+            }
+
+            //color cell Red:
+            var childGrid = new Grid {Background = Brushes.Red};
+            SetColumn(childGrid, col);
+            SetRow(childGrid, row);
+            Children.Add(childGrid);
         }
     }
 }
